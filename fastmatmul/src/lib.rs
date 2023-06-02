93f86f7ea160170018368fd44f18fbe13f694149
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-06-02 13:52:32 +0200

    next logical improvements

--- fastmatmul/src/lib.rs
+++ fastmatmul/src/lib.rs
@@ -12,9 +12,40 @@ pub fn matmul1(a: Vec<Vec<f64>>, b: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
 }
 
 /// Next improvement: Use references
-
+pub fn matmul2(a: &Vec<Vec<f64>>, b: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+    let mut result = vec![vec![0.0; 3]; 3];
+    for i in 0..3 {
+        for j in 0..3 {
+            for k in 0..3 {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    result
+}
 
 /// Next improvement: Use stack
+fn matmul3(a: &[[f64; 3]; 3], b: &[[f64; 3]; 3], result: &mut [ [f64; 3]; 3]) {
+    for i in 0..3 {
+        for j in 0..3 {
+            for k in 0..3 {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+}
+
+const A: [[f64; 3]; 3] = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]];
+const B: [[f64; 3]; 3] = [[9.0, 8.0, 7.0], [6.0, 5.0, 4.0], [3.0, 2.0, 1.0]];
+const C: [[f64; 3]; 3] = [[4.0, 5.0, 6.0], [7.0, 8.0, 9.0], [1.0, 2.0, 3.0]];
+
+fn driver() {
+    let mut res1: [[f64; 3]; 3] = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]];
+    let mut res2: [[f64; 3]; 3] = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]];
+    matmul3(&A, &B, &mut res1);
+    matmul3(&res1, &C, &mut res2);
+}
+
 
 #[cfg(test)]
 mod tests {
