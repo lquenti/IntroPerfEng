ef269ab126047d6a82b742dd34ea4ec2f4167929
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-09-16 16:20:10 +0200

    chapter 3 (results still computing)

---
+++ report/assets/variadic_unoptimized.rs
@@ -0,0 +1,12 @@
+fn matmul(a: Vec<Vec<f32>>, b: Vec<Vec<f32>>) -> Vec<Vec<f32>> {
+    let n = a.len();
+    let mut result = vec![vec![0.0; n]; n];
+    for i in 0..n {
+        for j in 0..n {
+            for k in 0..n {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    result
+}
